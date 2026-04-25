e3a2dd365c93c858aaed4f199b39ebcd965a3647
admin <admin@MacBook-Pro-Ryan.local>
2026-04-25 14:52:13 +0200

    ajout du header via app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
+import Header from './components/Header.jsx'
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
