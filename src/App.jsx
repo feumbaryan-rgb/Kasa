e7596b31f0e8f3fc0fccdadff81cc5a71a83f4c5
admin <admin@MacBook-Pro-Ryan.local>
2026-04-25 15:08:38 +0200

    ajout de l'import de about.jsx via app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
 import Header from './components/Header.jsx'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   )
