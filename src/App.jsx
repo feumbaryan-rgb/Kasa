92921e588a91ea280ff0c0e774a704c53f0a63af
admin <admin@MacBook-Pro-Ryan.local>
2026-05-14 22:21:04 +0200

    importation de logement.jsx dans app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+import Logement from './pages/Logement.jsx'
 
 function App() {
   return (
