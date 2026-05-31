8b502c80ceff4db68e77d255de673695bd6ab6c7
admin <admin@MacBook-Pro-Ryan.local>
2026-05-31 12:08:34 +0200

    mise ajour du contenue de la page about ainsi que sa stylisation via main.scss

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,31 @@
+import Collapse from '../components/Collapse'
+
 function About() {
-    return (
-      <main>
-        <h1>À propos</h1>
-      </main>
-    )
-  }
-  
-  export default About
+  return (
+    <main>
+      <div className="about">
+        <Collapse
+          title="Fiabilité"
+          content="Les annonces postées sur Kasa garantissent une fiabilité totale."
+        />
+
+        <Collapse
+          title="Respect"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa."
+        />
+
+        <Collapse
+          title="Service"
+          content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite."
+        />
+
+        <Collapse
+          title="Sécurité"
+          content="La sécurité est la priorité de Kasa."
+        />
+      </div>
+    </main>
+  )
+}
+
+export default About
